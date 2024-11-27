dc52afb1a835c6eb6d769ea6146a0140813e6a7e
Tixier Julien <tixier.julien60@yahoo.fr>
2024-11-27 11:28:56 +0100

    fix footer

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Insription", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Insription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
